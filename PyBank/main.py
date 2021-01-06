8d52b7dab75aa7e13ad15fa4559988f977816fb4
ruthhinkle <74269435+ruthhinkle@users.noreply.github.com>
2021-01-06 12:05:17 -0600

    election results added, cleaned up comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,30 +1,18 @@
-#Calculate each of the following:
-    #The total number of months included in the dataset
-    #The net total amount of "Profit/Losses" over the entire period
-    #The average of the changes in "Profit/Losses" over the entire period
-    #The greatest increase in profits (date and amount) over the entire period
-    #The greatest decrease in losses (date and amount) over the entire period
-    #As an example, your analysis should look similar to the one below:
-        #Financial Analysis
-        #----------------------------
-        #Total Months: 86
-        #Total: $38382578
-        #Average  Change: $-2315.12
-        #Greatest Increase in Profits: Feb-2012 ($1926159)
-        #Greatest Decrease in Profits: Sep-2013 ($-2196167)
-#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+#PYBANK CHALLENGE
 
-#import os & CSV
+#Import os & CSV
 import os
 import csv
 
-#set file path
+#Set file path
 csvpath = os.path.join("Resources", "budget_data.csv")
+
+#Create list for dates & profits/losses
 dates = []
 profit_losses = []
 
 #READ & INTERPRET THE DATA
-#open the CSV
+#Open the CSV
 with open(csvpath, mode= "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
@@ -36,8 +24,7 @@ with open(csvpath, mode= "r") as csvfile:
 sum_changes = 0
 month_count = 0
 
-#Loop through rows
-#Source: Enumerate function suggested by tutor
+#Loop through rows, SOURCE: enumerate function suggested by tutor
     for i,row in enumerate(csvdata):
             
         #sum count of months
@@ -57,7 +44,6 @@ month_count = 0
         dates.append(row[0])
 
 #DEFINE WHAT TO PRINT
-#print # of months
 analysis_text = (
     "Financial Analysis \n"
     "------------------------------ \n"
@@ -68,10 +54,11 @@ analysis_text = (
     f"Greatest Decrease in Losses: {dates[profit_losses.index(min(profit_losses))]} ${min(profit_losses)} \n"
 
     )
-#print analysis text
-print(analysis_text)
 
 #PRINT EVERYTHING
-#print to text file
+#Print analysis text
+print(analysis_text)
+
+#Print to text file
 with open("Analysis/analysis.txt", mode= "w") as txtfile:
     txtfile.write(analysis_text)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,35 +1,18 @@
-# Your task is to create a Python script that analyzes the votes and calculates each of the following:
-    # The total number of votes cast
-    # A complete list of candidates who received votes
-    # The percentage of votes each candidate won
-    # The total number of votes each candidate won
-    # The winner of the election based on popular vote.
-# As an example, your analysis should look similar to the one below:
-    # Election Results
-    # -------------------------
-    # Total Votes: 3521001
-    # -------------------------
-    # Khan: 63.000% (2218231)
-    # Correy: 20.000% (704200)
-    # Li: 14.000% (492940)
-    # O'Tooley: 3.000% (105630)
-    # -------------------------
-    # Winner: Khan
-    # -------------------------
-# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+#PYPOLL CHALLENGE
+
 #import os & CSV
 import os
 import csv
 
 #set file path
 csvpath = os.path.join("Resources", "election_data.csv")
-voter_ID = []
-county = []
-#candidate = [2]
-
 
 #READ / INTERPRET
-#open the CSV
+
+#Create Dictionary for Candidates
+candidate_count = {}
+
+#Open the CSV
 with open(csvpath, mode= "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
@@ -37,49 +20,33 @@ with open(csvpath, mode= "r") as csvfile:
     header = next(csvreader)
     csvdata = list(csvreader)
 
-#set voter count & candidate_count to zero
+#Initialize count of total votes to zero
 voter_count = 0
-# candidate_count = 0
 
-#loop through rows
+#Loop through the rows, SOURCE: enumerate function suggested by tutor
 for i,row in enumerate(csvdata):
 
-    #count the total votes
+    #Count the total number of votes
     voter_count += 1
     
-    #Add candidates names to dictionary and count their total votes each
-    candidate_count = {}
+    #Add candidates to candidate_count dictionary and count the votes they received
     candidate = row [2]
-    if candidate in candidate_count.keys():
+    if candidate in list(candidate_count.keys()):
         candidate_count[candidate] += 1
     else:
         candidate_count[candidate] = 1
 
-    #Calculate percentage of the total vote for each candidate
-    percentage = []
-    for i in candidate_count:
-        percentage = (float(candidate_count[i]) / voter_count) * 100
-        results = (f"{i} {round(percentage,0)}%  ({candidate_count[i]})")
+    #Calculate percentage of votes per candidate and write results list to print
+    results = ""
+    for candidate in list(candidate_count.keys()):
+        c_percentage = (float(candidate_count[candidate]) / voter_count) * 100
+        c_results = str(f"{candidate} {round(c_percentage,0)}%  ({candidate_count[candidate]}) \n")
+        results += c_results 
 
-
-    
-    #identify election winner
-
-    #average changes
-    #try:
-        #sum_changes += int(csvdata[i+1][1]) - int(row[1])
-    #except:
-        #sum_changes += 0
-
-    #append total votes
-    #total_votes.append(int(((row[0]))
-    #dates.append(row[0])
-    #define list
-
+    #Determine the winner of the popular vote
     for key in candidate_count.keys():
         if candidate_count[key] == max(candidate_count.values()):
             winner = key
-
 
 #DEFINE WHAT TO PRINT
 analysis_text = (
@@ -87,15 +54,15 @@ analysis_text = (
     "------------------------- \n"
     f"Total Votes: {voter_count} \n"
     "------------------------- \n"
-    f"{results} \n"
+    f"{results}"
     "------------------------- \n"    
     f"Winner: {winner} \n"
 )
 
-# #PRINT EVERYTHING
-#print to terminal
+#PRINT EVERYTHING
+#Print to terminal
 print(analysis_text)
 
-#print to text file
+#Print to text file
 with open("Analysis/analysis.txt", mode= "w") as txtfile:
     txtfile.write(analysis_text)
